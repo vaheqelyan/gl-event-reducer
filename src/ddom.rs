b1eedd56c11dfa19961550cdafbc000fb98f68ef
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2020-11-19 23:10:40 +0400

    refactor: seperate input logic

--- src/ddom.rs
+++ src/ddom.rs
@@ -11,14 +11,6 @@ struct ElementData {
     value: String,
 }
 
-/*#[derive(Debug)]
-pub struct Input {
-    pub value: String,
-    pub cursor: usize,
-    pub cursor_pos: f32,
-    pub push_left: f32,
-}*/
-
 #[derive(Debug)]
 pub struct Ddom {
     data: HashMap<usize, ElementData>,
@@ -50,120 +42,25 @@ impl Ddom {
 
     pub fn input(&mut self, value: char, offset: usize, id: &usize, container: f32) {
         if let Some(data_element) = self.input_data.get_mut(&(id + 1)) {
-            let mut size: f32 = 0.0;
-
-            data_element.value.insert(data_element.cursor, value);
-
-            data_element.cursor += 1;
-
-            for (pos, c) in data_element.value.chars().enumerate() {
-                if pos < data_element.cursor {
-                    let measure = self.font.get(c.to_string());
-                    size = (size + (measure.advance * 0.07)).round();
-                }
-            }
-
-            let original = size;
-            size -= data_element.push_left;
-
-            data_element.cursor_pos = clamp(size, 0.0, container - 1.0);
-
-            let is_out_of_range =
-                !((original - container) - data_element.push_left).is_sign_negative();
-
-            data_element.push_left = if is_out_of_range {
-                original - container
-            } else {
-                data_element.push_left
-            };
-            data_element.stop_backspace = false;
+            data_element.input(value, offset, container, &mut self.font);
         }
     }
 
     pub fn cursor_right(&mut self, id: &usize, container: f32) {
         if let Some(data_element) = self.input_data.get_mut(&(id + 1)) {
-            let mut size: f32 = 0.0;
-
-            for (pos, c) in data_element.value.chars().enumerate() {
-                if pos < data_element.cursor + 1 {
-                    let measure = self.font.get(c.to_string());
-
-                    size = (size + (measure.advance * 0.07)).round();
-                }
-            }
-
-            let original = size;
-            size -= data_element.push_left;
-
-            data_element.cursor_pos = clamp(size, 0.0, container - 1.0);
-            data_element.cursor += 1;
-
-            let is_out_of_range =
-                !((original - container) - data_element.push_left).is_sign_negative();
-
-            data_element.push_left = if is_out_of_range {
-                original - container
-            } else {
-                data_element.push_left
-            };
+            data_element.cursor_right(container, &mut self.font);
         }
     }
 
     pub fn cursor_left(&mut self, id: &usize, container: f32) {
         if let Some(data_element) = self.input_data.get_mut(&(id + 1)) {
-            if data_element.cursor > 0 {
-                let mut size: f32 = 0.0;
-
-                for (pos, c) in data_element.value.chars().enumerate() {
-                    if pos < data_element.cursor - 1 {
-                        let measure = self.font.get(c.to_string());
-
-                        size = (size + (measure.advance * 0.07)).round();
-                    }
-                }
-
-                let original = size;
-                size -= data_element.push_left;
-
-                data_element.cursor_pos = clamp(size, 0.0, container - 1.0);
-                data_element.cursor -= 1;
-                data_element.push_left = if size.is_sign_negative() {
-                    let d = (data_element.push_left - original).abs();
-                    data_element.push_left - d
-                } else {
-                    data_element.push_left
-                };
-            }
+            data_element.cursor_left(container, &mut self.font);
         }
     }
 
     pub fn backspace(&mut self, id: &usize, container: f32) {
         if let Some(data_element) = self.input_data.get_mut(&(id + 1)) {
-            if !data_element.stop_backspace {
-                let new_cursor = clamp_min(data_element.cursor - 1, 0);
-                let c = data_element.value.remove(data_element.cursor - 1);
-
-                data_element.cursor -= 1;
-
-                let mut size: f32 = 0.0;
-                for (pos, c) in data_element.value.chars().enumerate() {
-                    if pos < data_element.cursor {
-                        let measure = self.font.get(c.to_string());
-                        size = (size + (measure.advance * 0.07)).round();
-                    }
-                }
-
-                let original = size;
-
-                let measure = self.font.get(c.to_string());
-                data_element.push_left =
-                    clamp_min(data_element.push_left - (measure.advance * 0.07), 0.0).floor();
-
-                size -= data_element.push_left;
-
-                data_element.cursor_pos = clamp(size, 0.0, container - 1.0).floor();
-                data_element.stop_backspace = data_element.cursor == 0;
-            }
+            data_element.backspace(container, &mut self.font);
         }
     }
 }
--- src/input/input_element.rs
+++ src/input/input_element.rs
@@ -1,3 +1,5 @@
+use crate::font::Font;
+use num_traits::{clamp, clamp_min};
 #[derive(Debug)]
 pub struct Input {
     pub value: String,
@@ -5,4 +7,112 @@ pub struct Input {
     pub cursor_pos: f32,
     pub push_left: f32,
     pub stop_backspace: bool,
+}
+
+impl Input {
+    pub fn input(&mut self, value: char, offset: usize, container: f32, font: &mut Font) {
+        let mut size: f32 = 0.0;
+
+        self.value.insert(self.cursor, value);
+
+        self.cursor += 1;
+
+        for (pos, c) in self.value.chars().enumerate() {
+            if pos < self.cursor {
+                let measure = font.get(c.to_string());
+                size = (size + (measure.advance * 0.07)).round();
+            }
+        }
+
+        let original = size;
+        size -= self.push_left;
+
+        self.cursor_pos = clamp(size, 0.0, container - 1.0);
+
+        let is_out_of_range = !((original - container) - self.push_left).is_sign_negative();
+
+        self.push_left = if is_out_of_range {
+            original - container
+        } else {
+            self.push_left
+        };
+        self.stop_backspace = false;
+    }
+
+    pub fn cursor_right(&mut self, container: f32, font: &Font) {
+        let mut size: f32 = 0.0;
+
+        for (pos, c) in self.value.chars().enumerate() {
+            if pos < self.cursor + 1 {
+                let measure = font.get(c.to_string());
+
+                size = (size + (measure.advance * 0.07)).round();
+            }
+        }
+
+        let original = size;
+        size -= self.push_left;
+
+        self.cursor_pos = clamp(size, 0.0, container - 1.0);
+        self.cursor = self.cursor + 1;
+
+        let is_out_of_range = !((original - container) - self.push_left).is_sign_negative();
+
+        self.push_left = if is_out_of_range {
+            original - container
+        } else {
+            self.push_left
+        };
+    }
+
+    pub fn cursor_left(&mut self, container: f32, font: &Font) {
+        let mut size: f32 = 0.0;
+
+        for (pos, c) in self.value.chars().enumerate() {
+            if pos < self.cursor - 1 {
+                let measure = font.get(c.to_string());
+
+                size = (size + (measure.advance * 0.07)).round();
+            }
+        }
+
+        let original = size;
+        size -= self.push_left;
+
+        self.cursor_pos = clamp(size, 0.0, container - 1.0);
+        self.cursor -= 1;
+        self.push_left = if size.is_sign_negative() {
+            let d = (self.push_left - original).abs();
+            self.push_left - d
+        } else {
+            self.push_left
+        }
+    }
+
+    pub fn backspace(&mut self, container: f32, font: &Font) {
+        if !self.stop_backspace {
+            let new_cursor = clamp_min(self.cursor - 1, 0);
+            let c = self.value.remove(self.cursor - 1);
+
+            self.cursor -= 1;
+
+            let mut size: f32 = 0.0;
+            for (pos, c) in self.value.chars().enumerate() {
+                if pos < self.cursor {
+                    let measure = font.get(c.to_string());
+                    size = (size + (measure.advance * 0.07)).round();
+                }
+            }
+
+            let original = size;
+
+            let measure = font.get(c.to_string());
+            self.push_left = clamp_min(self.push_left - (measure.advance * 0.07), 0.0).floor();
+
+            size -= self.push_left;
+
+            self.cursor_pos = clamp(size, 0.0, container - 1.0).floor();
+            self.stop_backspace = self.cursor == 0;
+        }
+    }
 }
